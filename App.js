f7b1cd1f5c607ac635c0b3acbfb0e00966621825
AdityaSrivastava3107 <kallakallz123@gmail.com>
2023-06-04 15:09:12 +0530

    Added some styling and background to the game

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './Screens/StartGameScreen';
+import { StyleSheet, ImageBackground } from "react-native";
+import StartGameScreen from "./Screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen/>;
+  return (
+    <LinearGradient
+      colors={["#ddb52f", "#72063c"]}
+      style={styles.RootContainer}
+    >
+      <ImageBackground source={require('./assets/Images/Background.png')} resizeMode="contain" style={styles.ImageContainer} imageStyle={styles.ImageStyle}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
- 
+  RootContainer: {
+    backgroundColor: "#ddb52f",
+    flex: 1,
+  },
+  ImageContainer : {
+    flex: 1,
+  }, 
+  ImageStyle : {
+    marginTop : 70,
+    opacity : 0.5
+  }
 });
--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -3,36 +3,56 @@ import PrimaryButton from "../Components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.InputContainer}>
-      <TextInput style={styles.TextInputContainer} maxLength={2} keyboardType="number-pad"/>
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Add</PrimaryButton>
+      <TextInput
+        style={styles.TextInputContainer}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <View style={styles.ButtonStyle}>
+        <View style = {styles.SingleButton}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style = {styles.SingleButton}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-  InputContainer : {
-    marginHorizontal : 24,
-    padding : 16,
-    marginTop : 100,
-    backgroundColor : "#72063c",
-    borderRadius : 8,
-    shadowColor : 'black',
-    shadowOffset : {width : 5, height : 5}, 
-    shadowRadius : 6,
-    shadowOpacity : 0.5
+  InputContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 24,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 5, height: 5 },
+    shadowRadius: 6,
+    shadowOpacity: 0.5,
   },
-  TextInputContainer : {
-    height : 50,
-    fontSize : 32,
-    borderBottomColor : '#ddb52f',
-    borderBottomWidth : 2,
-    color : '#ddb52f',
-    fontWeight : 'bold',
-    marginVertical : 4,
-    width : 50,
-    height : 50, 
-    textAlign : 'center',
+  TextInputContainer: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    marginVertical: 4,
+    width: 50,
+    height: 50,
+    textAlign: "center",
+  },
+  ButtonStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 8,
+  },
+  SingleButton : {
+    flex : 1
   }
-})
+});
